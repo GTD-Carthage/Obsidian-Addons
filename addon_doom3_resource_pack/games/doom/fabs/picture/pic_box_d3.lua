e615255313e172ad6e6c0bbdf2299565927ca5f9
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-06-03 15:52:37 +0800

    - Added pic fabs using new 64px comp textures.

---
+++ addon_doom3_resource_pack/games/doom/fabs/picture/pic_box_d3.lua
@@ -0,0 +1,119 @@
+PREFABS.Pic_box_d3_single =
+{
+  file   = "picture/pic_box_d3.wad",
+  map    = "MAP01",
+
+  prob  = 25,
+  skip_prob = 25,
+
+  theme = "!hell",
+  env   = "building",
+
+  where  = "seeds",
+  seed_w = 2,
+  seed_h = 1,
+
+  height = 128,
+  deep   =  16,
+
+  x_fit = "frame",
+  y_fit = "top",
+
+  tex_D3BRCP1 =
+  {
+    D3BRCP1 = 1,
+    D3BRCP2 = 1,
+    D3BRCP3 = 1,
+    D3BRCP4 = 1,
+    D3BRCP5 = 1,
+    D3DKCP1 = 1,
+    D3DKCP2 = 1,
+    D3DKCP3 = 1,
+    D3DKCP4 = 1,
+    D3DKCP5 = 1,
+    D3DKCP6 = 1,
+    D3DKCP7 = 1,
+    D3DKCP8 = 1,
+    D3DKCP9 = 1,
+  }
+}
+
+PREFABS.Pic_box_d3_quad =
+{
+  template = "Pic_box_d3_single",
+  map = "MAP02",
+
+  height = 144,
+
+  tex_D3BRCP2 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3DKCP1 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3DKCP2 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  }
+}
+
+PREFABS.Pic_box_d3_hex =
+{
+  template = "Pic_box_d3_single",
+  map = "MAP03",
+
+  height = 144,
+
+  tex_D3BRCP2 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3BRCP3 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3DKCP1 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3DKCP2 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  },
+
+  tex_D3DKCP3 =
+  {
+    D3BRCP1 = 1, D3BRCP2 = 1, D3BRCP3 = 1, D3BRCP4 = 1,
+    D3BRCP5 = 1, D3DKCP1 = 1, D3DKCP2 = 1, D3DKCP3 = 1,
+    D3DKCP4 = 1, D3DKCP5 = 1, D3DKCP6 = 1, D3DKCP7 = 1,
+    D3DKCP8 = 1, D3DKCP9 = 1,
+  }
+}
