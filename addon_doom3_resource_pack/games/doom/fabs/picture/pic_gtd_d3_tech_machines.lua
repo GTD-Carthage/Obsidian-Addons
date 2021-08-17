1a87c455a738e4b8239d50e82ef52850287ad449
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-08-17 13:18:12 +0800

    - [D3 Resource Pack] Added new tech-themed pic fabs.

---
+++ addon_doom3_resource_pack/games/doom/fabs/picture/pic_gtd_d3_tech_machines.lua
@@ -0,0 +1,34 @@
+PREFABS.Pic_d3_machine_1 =
+{
+  file   = "picture/pic_gtd_d3_tech_machines.wad",
+  map    = "MAP01",
+
+  prob  = 25,
+  prob_skew = 1.5,
+  skip_prob = 25,
+
+  theme = "!hell",
+  env   = "building",
+
+  where  = "seeds",
+  seed_w = 2,
+  seed_h = 1,
+
+  height = 128,
+  deep   =  16,
+
+  x_fit = "frame",
+  y_fit = "top",
+}
+
+PREFABS.Pic_d3_machine_2 =
+{
+  template = "Pic_d3_machine_1",
+  map = "MAP02"
+}
+
+PREFABS.Pic_d3_machine_3 =
+{
+  template = "Pic_d3_machine_1",
+  map = "MAP03"
+}
