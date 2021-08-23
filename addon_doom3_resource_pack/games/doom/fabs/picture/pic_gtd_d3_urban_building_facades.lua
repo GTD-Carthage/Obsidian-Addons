8556d6cbcebae7a5b58d465b86c4e8ebca0b067d
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-08-23 10:47:36 +0800

    - [D3 Resource Pack] Added 4-seed wide variation for the building facade fab for urban.

--- addon_doom3_resource_pack/games/doom/fabs/picture/pic_gtd_d3_urban_building_facades.lua
+++ addon_doom3_resource_pack/games/doom/fabs/picture/pic_gtd_d3_urban_building_facades.lua
@@ -16,7 +16,7 @@ PREFABS.Pic_d3_building_facade_2x =
   height = 128,
   deep   =  16,
 
-  tex_D3HLITE1 =
+  tex_D3HLITE1 = -- main facade
   {
     D3HLITE1 = 1,
     D3HLITE2 = 1,
@@ -33,7 +33,7 @@ PREFABS.Pic_d3_building_facade_2x =
     D3HLITEE = 1,
   },
 
-  tex_D3MTGPL1 =
+  tex_D3MTGPL1 = -- highlights trims under the facade
   {
     D3MTBPL9 = 1,
     D3MTBPLA = 1,
@@ -48,7 +48,7 @@ PREFABS.Pic_d3_building_facade_2x =
     D3BMTL4 = 1,
   },
 
-  flat_CEIL3_4 =
+  flat_CEIL3_4 = -- lighting under the doors
   {
     CEIL3_4 = 1,
     FLAT2 = 1,
@@ -64,9 +64,11 @@ PREFABS.Pic_d3_building_facade_3x =
   template = "Pic_d3_building_facade_2x",
   map = "MAP02",
 
+  prob = 275,
+
   seed_w = 3,
 
-  tex_D3GHYL1 =
+  tex_D3GHYL1 = -- alt texture for the main facade
   {
     D3GHYL1 = 1,
     D3GHYL4 = 1,
@@ -113,5 +115,64 @@ PREFABS.Pic_d3_building_facade_3x =
     D3WMTL13 = 1,
   },
 
-  x_fit = { 10,14 , 138,142 , 266,270 }
+  x_fit = "frame",
+}
+
+PREFABS.Pic_d3_building_facade_4x =
+{
+  template = "Pic_d3_building_facade_2x",
+  map = "MAP03",
+
+  prob = 400,
+
+  seed_w = 4,
+
+  tex_D3GHYL1 = -- alt texture for the main facade
+  {
+    D3GHYL1 = 1,
+    D3GHYL4 = 1,
+    D3GHYL7 = 1,
+    D3GHYL8 = 1,
+    D3GHYL9 = 1,
+    D3GHYL10 = 1,
+    D3GHYL11 = 1,
+
+    D3MTBPL3 = 1,
+    D3MTBPL4 = 1,
+    D3MTBPL5 = 1,
+    D3MTBPL6 = 1,
+    D3MTBPL7 = 1,
+    D3MTBPL8 = 1,
+    D3MTBPL9 = 1,
+    D3MTBPLA = 1,
+    D3MTBPLB = 1,
+    D3MTBPLC = 1,
+    D3MTBPLD = 1,
+    D3MTBPLE = 1,
+    D3MTBPLJ = 1,
+
+    D3MTGPL1 = 1,
+    D3MTGPL2 = 1,
+
+    D3MTHUL7 = 1,
+    D3MTSD1 = 1,
+  
+    D3WMETL1 = 1,
+    D3WMETL4 = 1,
+    D3WMETL6 = 1,
+
+    D3WMTL1 = 1,
+    D3WMTL2 = 1,
+    D3WMTL3 = 1,
+    D3WMTL4 = 1,
+    D3WMTL6 = 1,
+    D3WMTL7 = 1,
+    D3WMTL8 = 1,
+    D3WMTL9 = 1,
+    D3WMTL11 = 1,
+    D3WMTL12 = 1,
+    D3WMTL13 = 1,
+  },
+
+  x_fit = "frame",
 }
