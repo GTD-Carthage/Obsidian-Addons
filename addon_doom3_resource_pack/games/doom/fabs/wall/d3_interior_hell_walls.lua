05bac6549772fc1dd0f8211035820494f61ab8ea
Tobby "GTD-Carthage" Ong <gtdcarthage@gmail.com>
2021-08-23 09:09:44 +0800

    - [D3 Resource Pack] More variation for the hell tablets wall set.

--- addon_doom3_resource_pack/games/doom/fabs/wall/d3_interior_hell_walls.lua
+++ addon_doom3_resource_pack/games/doom/fabs/wall/d3_interior_hell_walls.lua
@@ -11,18 +11,18 @@ PREFABS.Wall_d3_hell_skintek_1 = --#
   height = 128,
   deep = 16,
 
-  bound_z1 = 0,
-  bound_z2 = 128,
-
-  z_fit = "top",
-
   tex_D3SKINW2 =
   {
     D3SKINW1 = 5,
     D3SKINW2 = 5,
     D3SKINW3 = 1,
     D3SKINW4 = 1,    
-  }
+  },
+
+  bound_z1 = 0,
+  bound_z2 = 128,
+
+  z_fit = "top"
 }
 
 PREFABS.Wall_d3_hell_skintek_2 =
@@ -43,44 +43,18 @@ PREFABS.Wall_d3_hell_skintek_diag =
 
 PREFABS.Wall_d3_hell_tablets =
 {
-  template = "Wall_d3_hell_skintek_1",
+  file = "wall/d3_interior_hell_walls.wad",
   map = "MAP04",
 
   rank = 2,
   engine = "zdoom",
 
+  where = "edge",
   height = 129,
-  bound_z2 = 129,
-  z_fit = "top",
+  deep = 16,
 
   group = "d3_hell_tablets",
 
-  tex_D3SKINW2 = "D3SKINW2",
-  tex_D3HTBLT1 = {
-    D3HTBLT1 = 1,
-    D3HTBLT2 = 1,
-    D3HTBLT3 = 1,
-    D3HTBLT4 = 1,
-    D3HTBLT5 = 1,
-  }
-}
-
-PREFABS.Wall_d3_hell_tablets_no_slope =
-{
-  template = "Wall_d3_hell_skintek_1",
-
-  map = "MAP04",
-
-  rank = 1,
-  engine = "!zdoom",
-
-  height = 129,
-  bound_z2 = 129,
-  z_fit = "top",
-
-  group = "d3_hell_tablets",
-
-  tex_D3SKINW2 = "D3SKINW2",
   tex_D3HTBLT1 = {
     D3HTBLT1 = 1,
     D3HTBLT2 = 1,
@@ -88,6 +62,37 @@ PREFABS.Wall_d3_hell_tablets_no_slope =
     D3HTBLT4 = 1,
     D3HTBLT5 = 1,
   },
+
+  bound_z1 = 0,
+  bound_z2 = 129,
+
+  z_fit = "top"
+}
+
+PREFABS.Wall_d3_hell_tablets_no_slope =
+{
+  template = "Wall_d3_hell_tablets",
+  map = "MAP04",
+
+  rank = 1,
+  engine = "!zdoom",
+
+  line_344 = 0
+}
+
+PREFABS.Wall_d3_hell_tablets_alt =
+{
+  template = "Wall_d3_hell_tablets",
+  map = "MAP05",
+}
+
+PREFABS.Wall_d3_hell_tablets_alt_no_slope =
+{
+  template = "Wall_d3_hell_tablets",
+  map = "MAP05",
+
+  rank = 1,
+  engine = "!zdoom",
 
   line_344 = 0
 }
